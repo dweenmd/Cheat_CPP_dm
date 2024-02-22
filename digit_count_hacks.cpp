5e6c40824a7c5dcc095e97980ef55bfe62beef9c
Dween Mohammad <dween.md01@gmail.com>
2024-02-22 12:56:47 +0600

    digit count

---
+++ digit_count_hacks.cpp
@@ -0,0 +1,15 @@
+/**!
+* author: dween_md
+* created: 2024-02-22    12:51:50
+!**/
+#include <bits/stdc++.h>
+#define ll long long int
+using namespace std;
+
+int main()
+{
+    ll n = 1247865989456789767;
+    cout << (int)log10(n) + 1 << endl;
+
+    return 0;
+}
