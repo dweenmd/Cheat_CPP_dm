179305ad0a6972c5103c94bfabaf8ef26e464e3f
Dween Mohammad <dween.md01@gmail.com>
2024-03-09 01:04:00 +0600

    hacks

---
+++ arrayHacks.cpp
@@ -0,0 +1,16 @@
+/**!
+* author: dween_md
+* created: 2024-03-08    20:32:49
+!**/
+#include <bits/stdc++.h>
+using namespace std;
+int arr[100];
+int main()
+{
+    for(int i=0;i<100;i++)
+    {
+        cout<<arr[i]<<" ";//array globaly declare korar karone sob 0 dara assing hoise
+    }
+    
+    return 0;
+}
---
+++ binaryConvertWithSTL.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // bitset
+    cout << bitset<4>(5); // 5 er binary 4 digit porjonto
+    
+}
